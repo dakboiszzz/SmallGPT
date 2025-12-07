61beb241a228b7c9e3aa4a5a9c5e483fe5507101
Blackbi <ongbivlog3107@gmail.com>
2025-12-07 09:17:15 +0700

    Get batches

--- train.py
+++ train.py
@@ -24,6 +24,14 @@ words_val = words[n:]
 train_data = DataPrep(words_train).getData()
 val_data = DataPrep(words_val).getData()
 
+# Get batch
+def get_batch(split):
+    data = train_data if split == 'train' else val_data
+    ix = torch.randint(len(data)-config['block_size'], (config['batch_size'],))
+    x = torch.stack([data[i:i + config['block_size']] for i in ix])
+    y = torch.stack([data[i+1:i + config['block_size']+1] for i in ix])
+    return x,y
+
 
 
 
